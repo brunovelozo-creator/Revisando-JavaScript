4ccf7f662b2e2f4f3fdbecadd7097841686659ac
BrunoAugusto <bruno.velozo@aluno.senai.br>
2026-03-03 14:24:38 -0300

    Atividade finalizada por Bruno Augusto Verissimo Velozo

--- revisao2.js
+++ revisao2.js
@@ -1,5 +1,11 @@
 let produtos = ["Arroz", "Feijão", "Açúcar", "Macarrão", "Alface"];
 
+console.log("Todos os produtos:");
+for (let i = 0; i < produtos.length; i++) {
+    console.log(produtos[i]);
+}
+
+console.log("Produtos que começam com A:");
 for (let i = 0; i < produtos.length; i++) {
     if (produtos[i].startsWith("A")) {
         console.log(produtos[i]);
